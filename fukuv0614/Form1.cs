03a010c030478bd1bc3ba3388520f02696810e84
sakamoto-dat122 <167387815+sakamoto-dat122@users.noreply.github.com>
2024-06-14 13:44:41 +0900

    復習問題

--- fukuv0614/Form1.cs
+++ fukuv0614/Form1.cs
@@ -1,10 +1,40 @@
 namespace fukuv0614
 {
+
     public partial class Form1 : Form
     {
+        static Random random = new Random();
+        int count = 0;
+        int ans;
+
         public Form1()
         {
             InitializeComponent();
+            int ans = random.Next(51);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            count++;
+            MessageBox.Show($"{count}‰ñ–Ú‚Ì’§í");
+
+            int yosou;
+            yosou = int.Parse(textBox1.Text);
+
+            if (ans == yosou)
+            {
+                MessageBox.Show("Bingo!!");
+            }
+            else if (yosou >= ans)
+            {
+                MessageBox.Show("Greater");
+            }
+            else
+            {
+                MessageBox.Show("Less");
+            }
         }
     }
 }
